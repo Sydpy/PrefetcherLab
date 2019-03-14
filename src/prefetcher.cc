64213bebe72b72bf55fed14139e748a55a20356e
Sydpy <sydpy@pm.me>
2019-03-14 16:44:24 +0100

    Fixed broken logic

--- src/prefetcher.cc
+++ src/prefetcher.cc
@@ -6,168 +6,151 @@
 
 #include <limits.h>
 #include <strings.h>
+#include <stdlib.h>
 #include "interface.hh"
 
 #ifndef MARKOV_TABLE_SIZE
-#define MARKOV_TABLE_SIZE 32
+#define MARKOV_TABLE_SIZE 2048
 #endif
 
 #ifndef MARKOV_TABLE_DEGREE
 #define MARKOV_TABLE_DEGREE 4
 #endif
 
-typedef Addr Tag; 
-inline Tag tag_of(Addr addr) {
-    return addr >> 6;
-}
+typedef struct {
+    Addr addr; 
+    uint8_t confidence; 
+} Prediction;
 
 typedef struct {
-    Tag    tag;
-    unsigned short confidence;
-} PredictionEntry;
-
-typedef struct {
-    Tag    miss_tag;
-    unsigned short timestamp;
-    PredictionEntry predictions[MARKOV_TABLE_DEGREE];
+    Addr addr;
+    uint16_t timestamp;
+    Prediction predictions[MARKOV_TABLE_DEGREE];
 } MarkovEntry;
 
 
 MarkovEntry markov_table[MARKOV_TABLE_SIZE] = {0};
-int markov_table_size = 0;
-unsigned short timestamp = 0;
-    
+uint32_t markov_table_size = 0;
+uint16_t timestamp = 0;
+MarkovEntry* prev_entry = NULL;
 
-Tag prev_tag;
+
+int compare_pred(Prediction* p1, Prediction* p2) {
+    return p1->confidence - p2->confidence;
+}
+
 
 /* Get the index of the Markov entry for this address */
-MarkovEntry* get_entry(Tag tag) {
-    for (int i = 0; i < MARKOV_TABLE_SIZE; i++) {
-        if (markov_table[i].miss_tag == tag)
+MarkovEntry* get_entry(Addr addr) {
+    uint32_t i;
+    for (i = 0; i < markov_table_size; i++) {
+        if (markov_table[i].addr == addr)
             return &markov_table[i];
     }
 
     return NULL;
 }
 
-/* Get the predicted addr, the one with the greater confidence */
-Tag make_prediction(Tag tag) {
-    
-    MarkovEntry* entry = get_entry(tag);
-    unsigned short max_confidence = 0;
-    Tag prediction;
-
-    if (!entry) return 0;
-
-    int i;
-    for (i = 0; i < MARKOV_TABLE_DEGREE; i++) {
-        if (entry->predictions[i].confidence >= max_confidence) {
-            prediction = entry->predictions[i].tag;
-            max_confidence = entry->predictions[i].confidence;
-        }
-    }
-    
-    return prediction;
-}
 
 /* Create an entry inside the table (replace one if necessary) */
-MarkovEntry* new_entry(Tag tag) {
+MarkovEntry* new_entry(Addr addr) {
     
-    int to_replace_index = markov_table_size;
-    unsigned short lru_timestamp = USHRT_MAX;
-    int i, lru_index;
+    MarkovEntry* entry;
+    uint32_t insert_at = markov_table_size;
+    uint16_t lru_timestamp = UINT16_MAX;
+    uint32_t i, lru_index;
 
-    /* If the table it's full find the LRU to replace it */
+    /* If the table is full, replace the lru entry */
     if (markov_table_size == MARKOV_TABLE_SIZE) {
-        
-        for (i = 0; i < MARKOV_TABLE_SIZE; i++) {
+        for(i = 0; i < MARKOV_TABLE_SIZE; i++) {
             if (lru_timestamp > markov_table[i].timestamp) {
                 lru_timestamp = markov_table[i].timestamp;
                 lru_index = i;
             }
         }
-
-        to_replace_index = lru_index;
-
+        insert_at = lru_index;
     } else {
         markov_table_size++;
     }
 
-    markov_table[to_replace_index].miss_tag = tag;
-    markov_table[to_replace_index].timestamp = timestamp;
-    bzero(markov_table[to_replace_index].predictions, MARKOV_TABLE_DEGREE*sizeof(PredictionEntry));
+    entry = &markov_table[insert_at];
+    entry->addr = addr;
+    entry->timestamp = timestamp;
+    bzero(entry->predictions, MARKOV_TABLE_DEGREE*sizeof(Prediction));
 
-    return &markov_table[to_replace_index];
+    return entry;
 }
 
 /* Update the Markov table */
-void update_table(Tag miss_tag) {
-    
-    MarkovEntry* prev_entry = get_entry(prev_tag);
-    int i, least_confidence_index = 0;
-    unsigned short least_confidence;
+void update_table(MarkovEntry* entry, Addr addr) {
 
+    uint32_t update_at = 0;
+    uint8_t least_confidence = UINT8_MAX;
+    uint8_t i, least_confidence_index;
 
-    if (timestamp == USHRT_MAX) {
-        bzero(markov_table, sizeof(markov_table));
-        markov_table_size = 0;
-    }
-
-    timestamp++;
-
-    if (!prev_entry) {
-        prev_entry = new_entry(prev_tag);
-    } else {
-        for (i = 0; i < MARKOV_TABLE_DEGREE; i++) {
-            if (least_confidence > prev_entry->predictions[i].confidence) {
-                least_confidence = prev_entry->predictions[i].confidence;
-                least_confidence_index = i;
-            }
-        }
-    }
-    
     for (i = 0; i < MARKOV_TABLE_DEGREE; i++) {
-        
-        if (prev_entry->predictions[i].tag == miss_tag) {
             
-            if (prev_entry->predictions[i].confidence < USHRT_MAX)
-                prev_entry->predictions[i].confidence++;
-
+        /* Look if the current tag is a candidate prediction */
+        if (entry->predictions[i].addr == addr) {
+            update_at = i;
             break;
         }
+        /* Also look for the least confident entry in case we have
+         * to replace it */
+        if (least_confidence > entry->predictions[i].confidence) {
+            least_confidence = entry->predictions[i].confidence;
+            least_confidence_index = i;
+        }
+    }
+        
+    /* if we didn't find the entry, replace the least confident */
+    if (i == MARKOV_TABLE_DEGREE) {
+        update_at = least_confidence_index;
+        entry->predictions[update_at].confidence = 0;
     }
 
-    if (i == MARKOV_TABLE_DEGREE) {
-        prev_entry->predictions[least_confidence_index] = {miss_tag, 0};
+    entry->predictions[update_at].addr = addr;
+    if (entry->predictions[update_at].confidence < UINT8_MAX) {
+        entry->predictions[update_at].confidence++;
     }
-    
-    prev_entry->timestamp = timestamp;
-    prev_tag = miss_tag;
+
+    qsort(entry->predictions, MARKOV_TABLE_DEGREE, sizeof(Prediction), compare_pred);
 }
 
 void prefetch_init(void)
 {
     /* Called before any calls to prefetch_access. */
     /* This is the place to initialize data structures. */
-
-    DPRINTF(HWPrefetch, "Initialized Markov prefetcher\n");
 }
 
 void prefetch_access(AccessStat stat)
 {
-    Tag miss, prediction;
+    Addr addr = stat.mem_addr;
+    MarkovEntry* entry = get_entry(addr);
+    Prediction pred;
+    int8_t i;
 
     if (stat.miss) {
+
+        DPRINTF(HWPrefetch, "Missed %#x\n", addr);
+
+        if (!entry) {
+            entry = new_entry(addr);
+        } else {
         
-        miss = tag_of(stat.mem_addr);
-
-        /* Make a predicition and prefetch it if necessary */
-        prediction = make_prediction(miss);
-        if (prediction && !in_cache(prediction)) {
-            issue_prefetch(prediction);
+            for (i = MARKOV_TABLE_DEGREE-1; i > -1; i--) {
+                pred = entry->predictions[i];
+                if (pred.confidence > 0 && !in_cache(pred.addr)) {
+                    DPRINTF(HWPrefetch, "Prefetching %#x\n", pred.addr);
+                    issue_prefetch(pred.addr);
+                }
+            }
         }
+    
+        if (prev_entry)
+            update_table(prev_entry, addr);
 
-        update_table(miss);
+        prev_entry = entry;
     }
 }
 
@@ -175,4 +158,5 @@ void prefetch_complete(Addr addr) {
     /*
      * Called when a block requested by the prefetcher has been loaded.
      */
+    set_prefetch_bit(addr);
 }
